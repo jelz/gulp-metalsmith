f3c42090a772a7050b697386cd450a2a8cea53e2
Jakub Elżbieciak <jelz@post.pl>
2016-02-27 14:27:49 +0100

    Refactor Metalsmith module

--- test/metalsmith.js
+++ test/metalsmith.js
@@ -1,79 +1,116 @@
 var test = require('tape');
+var path = require('path');
 var _ = require('lodash');
+
 var metalsmith = require('../lib/metalsmith.js');
 
-test('Metalsmith API', function(t) {
-    var methods = ['build', 'source', 'destination', 'clean', 'frontmatter', 'use', 'run', 'metadata', 'path'];
-    var m = metalsmith();
-    t.plan(methods.length);
-    methods.forEach(function(k) { t.true(_.isFunction(m[k])); });
+test('Metalsmith-compatible API', function (t) {
+  var API = [
+    'build',
+    'source',
+    'destination',
+    'clean',
+    'frontmatter',
+    'use',
+    'run',
+    'metadata',
+    'path'
+  ];
+
+  var m = metalsmith();
+  t.plan(API.length);
+
+  API.forEach(function (method) {
+    t.true(_.isFunction(m[method]));
+  });
 });
 
-test('Setter chaining', function(t) {
-    var methods = ['source', 'destination', 'clean', 'frontmatter', 'metadata'];
-    var m = metalsmith();
-    t.plan(methods.length);
-    methods.forEach(function(k) { t.equal(m, m[k]({})); });
+test('Not implemented methods', function (t) {
+  var m = metalsmith();
+  var NOT_IMPLEMENTED = metalsmith.NOT_IMPLEMENTED;
+  t.plan(NOT_IMPLEMENTED.length);
+
+  NOT_IMPLEMENTED.forEach(function (method) {
+    t.throws(m[method]);
+  });
 });
 
-test('Metadata get/set', function(t) {
-    var m = metalsmith();
-    var value = { test: 123, nested: { msg: 'Hello' } };
-    t.deepEqual(m.metadata(), {});
-    t.equal(m.metadata(value), m);
-    t.equal(m.metadata(), value);
-    t.deepEqual(m.metadata(), { test: 123, nested: { msg: 'Hello' } });
-    t.end();
+test('Metadata getter/setter', function (t) {
+  var m = metalsmith();
+  var value = {test: 123, nested: {msg: 'Hello'}};
+  t.deepEquals(m.metadata(), {});
+  t.equals(m.metadata(value), m);
+  t.equals(m.metadata(), value);
+  t.deepEquals(m.metadata(), {test: 123, nested: {msg: 'Hello'}});
+  t.end();
 });
 
-test('Build stub method', function(t) {
-    var m = metalsmith();
-    t.plan(2);
-    m.build(function(err, files) {
-        t.true(_.isNull(err));
-        t.equal(files.length, 0);
-    });
+test('Path getter', function (t) {
+  var cwd = process.cwd();
+  var m = metalsmith();
+  t.equals(m.path(), cwd);
+  t.equals(m.path('../hello.txt'), path.resolve(cwd, '..', 'hello.txt'));
+  m = metalsmith('/tmp/metalsmith');
+  t.equals(m.path(), '/tmp/metalsmith');
+  t.equals(m.path('test/hello.txt'), '/tmp/metalsmith/test/hello.txt');
+  t.equals(m.path('../../test.html'), '/test.html');
+  t.end();
 });
 
-test('Path getter', function(t) {
-    var m = metalsmith();
-    t.equal(m.path(), process.cwd());
-    t.equal(m.path('../hello.txt'), require('path').resolve(process.cwd(), '..', 'hello.txt'));
-    m = metalsmith('/tmp/metalsmith');
-    t.equal(m.path(), '/tmp/metalsmith');
-    t.equal(m.path('test/hello.txt'), '/tmp/metalsmith/test/hello.txt');
-    t.equal(m.path('../../test.html'), '/test.html');
-    t.end();
+test('Middleware flow', function (t) {
+  var m = metalsmith();
+  t.plan(3);
+
+  m.use(function (files, m, next) {
+    t.pass();
+    next();
+  });
+  m.use(function (files, m, next) {
+    t.pass();
+    next();
+  });
+
+  m.run({}, function () {
+    t.pass();
+  });
 });
 
-test('Use flow', function(t) {
-    var m = metalsmith();
-    t.plan(3);
-    m.use(function(files, m, next) { t.pass(); next(); });
-    m.use(function(files, m, next) { t.pass(); next(); });
-    m.run({}, function() { t.pass(); });
+test('Running middleware functions', function (t) {
+  var m = metalsmith();
+  t.plan(3);
+
+  m.use(function (files, m, next) {
+    files.added_file = {};
+    next();
+  });
+  m.use(function (files, m, next) {
+    m.metadata().added_metadata = 123;
+    next();
+  });
+
+  m.run({}, function (err, files) {
+    t.true(_.isNull(err));
+    t.true(_.isObject(files.added_file));
+    t.equals(m.metadata().added_metadata, 123);
+  });
 });
 
-test('Run middlewares', function(t) {
-    var m = metalsmith();
-    t.plan(3);
-    m.use(function(files, m, next) { files.added_file = {}; next(); });
-    m.use(function(files, m, next) { m.metadata().added_metadata = 123; next(); });
-    m.run({}, function(err, files) {
-        t.true(_.isNull(err));
-        t.true(_.isObject(files.added_file));
-        t.equal(m.metadata().added_metadata, 123);
-    });
-});
+test('Handling middleware errors', function (t) {
+  var m = metalsmith();
+  t.plan(2);
 
-test('Middleware errors', function(t) {
-    var m = metalsmith();
-    t.plan(2);
-    m.use(function(f, m, cb) { cb(); });
-    m.use(function(f, m, cb) { cb(new Error('Second middleware error.')); });
-    m.use(function(f, m, cb) { cb(new Error('Last middleware error.')); });
-    m.run({}, function(err) {
-        t.true(_.isError(err));
-        t.equal(err.message, 'Second middleware error.');
-    });
+  m.use(function (files, m, next) {
+    next();
+  });
+  m.use(function (files, m, next) {
+    next(new Error('Second middleware error.'));
+  });
+  m.use(function (files, m, next) {
+    next(new Error('Last middleware error.'));
+  });
+
+  m.run({}, function (err) {
+    t.true(_.isError(err));
+    t.equals(err.message, 'Second middleware error.');
+  });
 });
