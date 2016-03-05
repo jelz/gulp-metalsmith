80ae9dc6d197d16ec1ea54867e7a3352dd7602ff
Jakub Elżbieciak <jelz@post.pl>
2016-03-05 17:32:08 +0100

    Rewrite JSON definitions support

--- test/plugin.js
+++ test/plugin.js
@@ -9,19 +9,15 @@ var _ = require('lodash');
 var plugin = require('..');
 
 var base = _.partial(path.join, __dirname, 'fixtures');
-var prepare = _.partial(prepareFn, 'src');
-var prepareJson = _.partial(prepareFn, 'json');
 
-function prepareFn(dir, globs, opts) {
-  var fn = dir === 'json' ? plugin.json : plugin;
+function prepare(globs, opts) {
+  opts = _.extend({root: base()}, opts);
   globs = _.isString(globs) ? [globs] : globs;
   globs = _.map(globs, function (glob) {
-    return base(dir, glob || '');
+    return base(glob || '');
   });
 
-  opts = _.extend({root: base()}, opts);
-
-  return vinyl.src(globs).pipe(fn(opts));
+  return vinyl.src(globs).pipe(plugin(opts));
 }
 
 function testContent(f, path, str, fn) {
@@ -33,162 +29,145 @@ function testContent(f, path, str, fn) {
   }
 }
 
-test('Plugin API', function (t) {
-  t.true(_.isFunction(plugin));
-  t.true(_.isFunction(plugin.json));
+test('Plugin is a function that returns a stream', function (t) {
+  t.ok(_.isFunction(plugin));
+  var p = plugin();
+
+  ['emit', 'on', 'pipe', 'push', 'write', 'end'].forEach(function (m) {
+    t.ok(_.isFunction(p[m]));
+  });
   t.end();
 });
 
-test('Plugin returns a stream', function (t) {
-  var methods = ['emit', 'on', 'pipe', 'push', 'write', 'end'];
-  var p = plugin();
-  t.plan(methods.length);
-
-  methods.forEach(function (m) {
-    t.true(_.isFunction(p[m]));
-  });
-});
-
-test('Failure on a file containing a stream', function (t) {
+test('Plugin emits error for vinyl files of type "stream"', function (t) {
   var file = new gutil.File({contents: through.obj()});
   var s = plugin();
   t.plan(4);
 
   s.on('error', function (err) {
-    t.true(err instanceof gutil.PluginError);
-    t.equals(err.plugin, plugin.PLUGIN_NAME);
-    t.true(_.includes(err.message, 'not supported'));
+    t.ok(err instanceof gutil.PluginError);
+    t.equal(err.plugin, plugin.PLUGIN_NAME);
+    t.ok(_.includes(err.message, 'not supported'));
   });
 
-  t.true(file.isStream());
+  t.ok(file.isStream());
   s.end(file);
 });
 
-test('Handle a stream of buffered vinyl files', function (t) {
+test('Plugin handles a stream of vinyl files of type "buffer"', function (t) {
   t.plan(3);
-  prepare('**').pipe(through.obj(function (f, enc, cb) {
+  prepare('*').pipe(through.obj(function (f, enc, cb) {
     testContent(f, 'index.html', 'Index page', t.pass);
     testContent(f, 'contact.html', 'Contact page', t.pass);
     cb();
   }, t.pass));
 });
 
-test('Omit ignored files', function (t) {
-  t.plan(5);
-  prepare('**', {ignore: 'src/index.html', use: testSingle});
-  prepare('**', {ignore: ['src/**', '!src/*.jpg'], use: testMultiple});
+test('Plugin omits ignored files', function (t) {
+  t.plan(6);
+  prepare('*.html', {ignore: 'index.html', use: testSingle});
+  prepare('*', {ignore: ['*', '!*.jpg'], use: testMultiple});
 
   function testSingle(files) {
-    t.equals(_.values(files).length, 2);
-    t.equals(files['contact.html'].title, 'Contact');
-    t.ok(files['trees.jpg']);
+    t.equal(_.values(files).length, 1);
+    t.equal(files['contact.html'].title, 'Contact');
+    t.notOk(files['index.html']);
   }
 
   function testMultiple(files) {
-    t.equals(_.values(files).length, 1);
+    t.equal(_.values(files).length, 1);
     t.ok(files['trees.jpg']);
+    t.notOk(files['pc.png']);
   }
 });
 
-test('Do not touch non-utf8 files', function (t) {
-  t.plan(6);
-
-  prepare('*.jpg').pipe(through.obj(function (f, enc, cb) {
-    t.true(f.isBuffer());
-    t.true(f.contents.equals(fs.readFileSync(base('src', 'trees.jpg'))));
-    cb();
-  }, t.pass));
-
-  prepareJson('*.png').pipe(through.obj(function (f, enc, cb) {
-    t.true(f.isBuffer());
-    t.true(f.contents.equals(fs.readFileSync(base('json', 'pc.png'))));
-    cb();
-  }, t.pass));
-});
-
-test('Accept a single middleware function', function (t) {
-  t.plan(1);
-
-  prepare('index.html', {
-    use: function () {
-      t.pass();
-    }
-  });
-});
-
-test('Add metadata from configuration options', function (t) {
+test('Plugin does not touch non-utf8 files', function (t) {
   t.plan(3);
+  prepare('*.jpg').pipe(through.obj(function (f, enc, cb) {
+    t.ok(f.isBuffer());
+    t.ok(f.contents.equals(fs.readFileSync(base('trees.jpg'))));
+    cb();
+  }, t.pass));
+});
 
-  prepare('index.html', {
+test('Plugin accepts a single middleware function', function (t) {
+  t.plan(1);
+  prepare('index.html', {use: t.pass});
+});
+
+test('Plugin adds metadata items from configuration options', function (t) {
+  t.plan(3);
+  prepare('*.html', {
     metadata: {
       item1: true,
       item2: ['list', 'of', 'things']
     },
-    use: [testMiddleware]
+    use: [testMetadata]
   });
 
-  function testMiddleware(files, m, next) {
+  function testMetadata(files, m, next) {
     var meta = m.metadata();
-    t.true(meta.item1);
-    t.equals(meta.item2.length, 3);
-    t.equals(meta.item2[2], 'things');
+    t.ok(meta.item1);
+    t.equal(meta.item2.length, 3);
+    t.equal(meta.item2[2], 'things');
   }
 });
 
-test('Frontmatter configuration option', function (t) {
+test('Plugin parses frontmatter if opts.frontmatter is true', function (t) {
   t.plan(2);
   prepare('index.html').pipe(stillIncludes(false));
   prepare('contact.html', {frontmatter: false}).pipe(stillIncludes(true));
 
   function stillIncludes(method) {
     return through.obj(function (f, enc, cb) {
-      t[method ? 'true' : 'false'](_.includes(f.contents.toString(), '---'));
+      t[method ? 'ok' : 'notOk'](_.includes(f.contents.toString(), '---'));
       cb();
     });
   }
 });
 
-test('Failure when Metalsmith fails', function (t) {
-  var s = prepare('**', {
-    use: [function (f, m, next) {
-      next(new Error('boom!'));
-    }]
-  });
-
+test('Plugin fails when Metalsmith or middleware fails', function (t) {
   t.plan(2);
+  var s = prepare('**', {use: failingMiddleware});
 
   s.on('error', function (err) {
-    t.true(err instanceof gutil.PluginError);
-    t.equals(err.message, 'boom!');
-  });
-});
-
-test('Failure on an invalid JSON', function (t) {
-  t.plan(3);
-
-  prepareJson('invalid_page.json').on('error', isPluginError);
-  prepareJson('array.json').on('error', function (err) {
-    isPluginError(err);
-    t.true(_.includes(err.message, 'single root object'));
+    t.ok(err instanceof gutil.PluginError);
+    t.equal(err.message, 'boom!');
   });
 
-  function isPluginError(err) {
-    t.true(err instanceof gutil.PluginError);
+  function failingMiddleware(f, m, next) {
+    next(new Error('boom!'));
   }
 });
 
-test('Handle JSON input', function (t) {
+test('Plugin fails when JSON definitions are invalid', function (t) {
   t.plan(3);
-  prepareJson('pages.json').pipe(through.obj(function (f, enc, cb) {
+  prepare('**', {json: 'json/invalid.json'}).on('error', isPluginError);
+  prepare('**', {json: 'json/array.json'}).on('error', function (err) {
+    isPluginError(err);
+    t.ok(_.includes(err.message, 'single root object'));
+  });
+
+  function isPluginError(err) {
+    t.ok(err instanceof gutil.PluginError);
+  }
+});
+
+test('Plugin creates pages from JSON definitions', function (t) {
+  t.plan(3);
+  var s = prepare('**/*.json', {json: 'json/pages.json'});
+
+  s.pipe(through.obj(function (f, enc, cb) {
     testContent(f, 'index.html', 'Index page', t.pass);
     testContent(f, 'contact.html', 'Contact page', t.pass);
     cb();
   }, t.pass));
 });
 
-test('Handle a multi-file JSON input', function (t) {
-  var s = prepareJson(['pages.json', 'offer_page.json']);
+test('Plugin merges and creates pages from multiple JSON inputs', function (t) {
   t.plan(4);
+  var json = ['json/pages.json', 'json/offer_page.json'];
+  var s = prepare('**/*.json', {json: json});
 
   s.pipe(through.obj(function (f, enc, cb) {
     testContent(f, 'index.html', 'Index page', t.pass);
@@ -198,18 +177,35 @@ test('Handle a multi-file JSON input', function (t) {
   }, t.pass));
 });
 
-test('Handle a JSON defined page w/o contents', function (t) {
-  t.plan(2);
-  prepareJson('empty_page.json').pipe(through.obj(function (f) {
-    t.equals(f.path, 'empty.html');
-    t.equals(f.contents.toString(), '');
-  }));
+test('Plugin converts all JSON files if opts.json is true', function (t) {
+  t.plan(3);
+  var globs = ['json/pages.json', 'json/*_page.json'];
+  var s = prepare(globs, {json: true, use: testFiles});
+
+  function testFiles(files) {
+    t.equals(_.values(files).length, 5);
+    t.ok(files['index.html']);
+    t.ok(files['map.html']);
+  }
 });
 
-test('Ignore invalid file keys', function (t) {
+test('Plugin handles a JSON defined page w/o contents', function (t) {
+  t.plan(3);
+  var s = prepare('json/empty_page.json', {json: true});
+
+  s.pipe(through.obj(function (f, enc, cb) {
+    t.equal(f.path, 'empty.html');
+    t.equal(f.contents.toString(), '');
+    cb();
+  }, t.pass));
+});
+
+test('Plugin ignores invalid file keys in JSON definition', function (t) {
   t.plan(2);
-  prepareJson('map_page.json').pipe(through.obj(function (f, enc, cb) {
-    t.equals(f.path, 'map.html');
+  var s = prepare('json/map_page.json', {json: true});
+
+  s.pipe(through.obj(function (f, enc, cb) {
+    t.equal(f.path, 'map.html');
     cb();
   }, t.pass));
 });
